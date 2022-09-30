ff72e1aa3dcf088d8cf6a5039c7a453d7044dbf1
srivardhan123 <srivardhanrao9009@gmail.com>
2022-09-30 21:39:33 +0530

    read excel file,created octant_function

---
+++ tut03/tut03.py
@@ -0,0 +1,54 @@
+try:
+    import pandas as pd
+    import numpy as np
+    try:
+        #now i am reading the excel file using the pandas.
+        dataframe = pd.read_excel('input_octant_transition_identify.xlsx')
+        #i have stored the total no of +1,-1,2,-2,3,-3,..are present in the those variables.
+        x1 = 0
+        x2 = 0
+        x3 = 0
+        x4 = 0
+        y1 = 0
+        y2 = 0
+        y3 = 0
+        y4 = 0
+        #created this function to identify in which octant it belongs..
+        def octant_identification(val,val1,val2):
+            #using if and else conditons with 8conditions i have divided the octants value.
+            if (val>0 and val1>0 and val2>0):
+                global x1
+                #increasing each timee the value of x1 globally..similarly x2,x3,y1,y2...etc
+                x1+=1
+                return "+1"
+            elif (val<0 and val1>0 and val2>0):
+                global x2
+                x2+=1
+                return "+2"
+            elif (val<0 and val1<0 and val2>0):
+                global x3
+                x3+=1
+                return "+3"
+            elif (val>0 and val1<0 and val2>0):
+                global x4
+                x4+=1
+                return "+4"
+            elif (val>0 and val1>0 and val2<0):
+                global y1
+                y1+=1
+                return "-1"
+            elif (val<0 and val1>0 and val2<0):
+                global y2
+                y2+=1
+                return "-2"
+            elif (val<0 and val1<0 and val2<0):
+                global y3
+                y3+=1
+                return "-3"
+            elif (val>0 and val1<0 and val2<0):
+                global y4
+
+                y4+=1
+                return "-4"
+
+        #adding the columns accoridng it my use and intially keeping it empty.
