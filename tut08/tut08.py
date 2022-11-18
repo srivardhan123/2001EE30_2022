3ddd4d6bdd513dec45ea3c13cd0d6435ae3015f2
srivardhan123 <srivardhanrao9009@gmail.com>
2022-11-18 12:18:59 +0530

    input files added

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -3,12 +3,9 @@ from datetime import datetime
 start_time = datetime.now()
 
 #Help
-def octant_analysis(mod=5000):
+def scorecard():
 	pass
 
-##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
-##Save all the excel files in a the output/ folder. Only xlsx to be allowed
-## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
 
 ###Code
 
@@ -21,8 +18,7 @@ else:
 	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
-mod=5000
-octant_analysis(mod)
+scorecard()
 
 
 
